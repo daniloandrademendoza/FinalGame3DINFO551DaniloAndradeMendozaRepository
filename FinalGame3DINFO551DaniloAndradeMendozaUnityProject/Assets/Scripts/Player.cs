b325cccf43daea4120110a40e70971c93471ae36
Danilo Andrade Mendoza <daniloandradem10@email.arizona.edu>
2018-11-12 15:38:20 -0700

    Started writing the player script, the player can now move up, left, down, and right with the arrow keys and it turns to the direction that it is going.

---
+++ FinalGame3DINFO551DaniloAndradeMendozaUnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Rigidbody rigidBodyPlayer;
+    private float playerSpeed;
+	// Use this for initialization
+	void Start () {
+        rigidBodyPlayer = GetComponent<Rigidbody>();
+        playerSpeed = 2f;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBodyPlayer.velocity = new Vector3(rigidBodyPlayer.velocity.x,0f,playerSpeed);
+            this.transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBodyPlayer.velocity = new Vector3(-playerSpeed,0f,rigidBodyPlayer.velocity.z);
+            this.transform.eulerAngles = new Vector3(0f, -90f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidBodyPlayer.velocity = new Vector3(rigidBodyPlayer.velocity.x,0f,-playerSpeed);
+            this.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBodyPlayer.velocity = new Vector3(playerSpeed,0f,rigidBodyPlayer.velocity.z);
+            this.transform.eulerAngles = new Vector3(0f, 90f, 0f);
+        }
+	}
+}
