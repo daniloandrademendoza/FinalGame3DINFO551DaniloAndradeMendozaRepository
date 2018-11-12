dffd332e7e76917c0fb602626178c780d3ce4461
Danilo Andrade Mendoza <daniloandradem10@email.arizona.edu>
2018-11-12 16:19:53 -0700

    Implemented the walk, run, jump, flip, punch, and kick animations for the player in the player script.

--- FinalGame3DINFO551DaniloAndradeMendozaUnityProject/Assets/Scripts/Player.cs
+++ FinalGame3DINFO551DaniloAndradeMendozaUnityProject/Assets/Scripts/Player.cs
@@ -5,33 +5,96 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     private Rigidbody rigidBodyPlayer;
     private float playerSpeed;
+    private float playerSpeedRun;
+    private Animation animationPlayer;
 	// Use this for initialization
 	void Start () {
         rigidBodyPlayer = GetComponent<Rigidbody>();
-        playerSpeed = 2f;
+        playerSpeed = 5f;
+        playerSpeedRun = 10f;
+        animationPlayer = GetComponent<Animation>();
 	}
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            rigidBodyPlayer.velocity = new Vector3(rigidBodyPlayer.velocity.x,0f,playerSpeed);
-            this.transform.eulerAngles = new Vector3(0f,0f,0f);
+            if (Input.GetKey(KeyCode.R))
+            {
+                rigidBodyPlayer.velocity = new Vector3(rigidBodyPlayer.velocity.x, 0f, playerSpeedRun);
+                this.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                animationPlayer.Play("run");
+            }
+            else
+            {
+                rigidBodyPlayer.velocity = new Vector3(rigidBodyPlayer.velocity.x, 0f, playerSpeed);
+                this.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                animationPlayer.Play("walk");
+            }
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rigidBodyPlayer.velocity = new Vector3(-playerSpeed,0f,rigidBodyPlayer.velocity.z);
-            this.transform.eulerAngles = new Vector3(0f, -90f, 0f);
+            if(Input.GetKey(KeyCode.R))
+            {
+                rigidBodyPlayer.velocity = new Vector3(-playerSpeedRun, 0f, rigidBodyPlayer.velocity.z);
+                this.transform.eulerAngles = new Vector3(0f, -90f, 0f);
+                animationPlayer.Play("run");
+            }
+            else
+            {
+                rigidBodyPlayer.velocity = new Vector3(-playerSpeed, 0f, rigidBodyPlayer.velocity.z);
+                this.transform.eulerAngles = new Vector3(0f, -90f, 0f);
+                animationPlayer.Play("walk");
+            }
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            rigidBodyPlayer.velocity = new Vector3(rigidBodyPlayer.velocity.x,0f,-playerSpeed);
-            this.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            if(Input.GetKey(KeyCode.R))
+            {
+                rigidBodyPlayer.velocity = new Vector3(rigidBodyPlayer.velocity.x, 0f, -playerSpeedRun);
+                this.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+                animationPlayer.Play("run");
+            }
+            else
+            {
+                rigidBodyPlayer.velocity = new Vector3(rigidBodyPlayer.velocity.x, 0f, -playerSpeed);
+                this.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+                animationPlayer.Play("walk");
+            }
+           
+
         }
         else if(Input.GetKey(KeyCode.RightArrow))
         {
-            rigidBodyPlayer.velocity = new Vector3(playerSpeed,0f,rigidBodyPlayer.velocity.z);
-            this.transform.eulerAngles = new Vector3(0f, 90f, 0f);
+            if(Input.GetKey(KeyCode.R))
+            {
+                rigidBodyPlayer.velocity = new Vector3(playerSpeedRun, 0f, rigidBodyPlayer.velocity.z);
+                this.transform.eulerAngles = new Vector3(0f, 90f, 0f);
+                animationPlayer.Play("run");
+            }
+            else
+            {
+                rigidBodyPlayer.velocity = new Vector3(playerSpeed, 0f, rigidBodyPlayer.velocity.z);
+                this.transform.eulerAngles = new Vector3(0f, 90f, 0f);
+                animationPlayer.Play("walk");
+            }
         }
-	}
+        else if(Input.GetKey(KeyCode.J))
+        {
+            animationPlayer.Play("jump");
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            animationPlayer.Play("flip");
+        }
+        else if (Input.GetKey(KeyCode.P))
+        {
+            animationPlayer.Play("punch");
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            animationPlayer.Play("kick");
+        }
+        
+    }
 }
