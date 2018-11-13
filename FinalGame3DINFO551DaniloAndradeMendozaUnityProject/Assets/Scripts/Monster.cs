3e7fd63f9cc0d9827f7d6252f79107e33b2a51b3
Danilo Andrade Mendoza <daniloandradem10@email.arizona.edu>
2018-11-13 00:28:36 -0700

    I put a dark dragon inside the castle and began writing the Moving Monster and Monster scripts.

---
+++ FinalGame3DINFO551DaniloAndradeMendozaUnityProject/Assets/Scripts/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+    private int lifePoints;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ FinalGame3DINFO551DaniloAndradeMendozaUnityProject/Assets/Scripts/MoveMonster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MoveMonster : MonoBehaviour {
+    public float timeMove;
+    public LayerMask collisionLayer;
+    private BoxCollider boxColliderMonster;
+    private Rigidbody rigidBodyMonster;
+    private float inverseTimeMove;
+	// Use this for initialization
+	protected virtual void Start () {
+        timeMove = .1f;
+        boxColliderMonster = GetComponent<BoxCollider>();
+        rigidBodyMonster = GetComponent<Rigidbody>();
+        inverseTimeMove = 1f / timeMove;
+	}
+	protected bool MoveEnemy(int x, int y, int z, out RaycastHit rayCastHit)
+    {
+        Vector3 begin = transform.position;
+        Vector3 finish = begin + new Vector3(x, y, z);
+        boxColliderMonster.enabled = false;
+        rayCastHit = Physics.Linecast(begin, finish, collisionLayer);
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
