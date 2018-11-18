d092067a57f0ddf2aa07278a3e40fc11951f4f61
Danilo Andrade Mendoza <daniloandradem10@email.arizona.edu>
2018-11-17 18:04:07 -0700

    Implemented polymorphism, made the Dragon class the parent of the Dark Dragon classes, the Dragon class is abstract, contains all of the attributes and abstract methods for the Dark Dragon classes.

---
+++ FinalGame3DINFO551DaniloAndradeMendozaUnityProject/Assets/Scripts/DarkDragon1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkDragon1 : Dragon {
+    public override void Update () {
+        this.coroutine = WaitAndAttack(this.waitTime);
+        StartCoroutine(this.coroutine);
+        if (!this.animatorMonster.GetCurrentAnimatorStateInfo(0).IsName("atk01"))
+        {
+            this.boxColliderDragon.size = new Vector3(this.boxColliderDragon.size.x, this.boxColliderDragon.size.y,this.zStartBoxCollider);
+        }
+    }
+    public override IEnumerator WaitAndAttack(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        this.animatorMonster.Play("atk01");
+        this.boxColliderDragon.size = new Vector3(this.boxColliderDragon.size.x,this.boxColliderDragon.size.y,this.zNewBoxCollider);
+    }
+
+    public override void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "MAX" && (collision.gameObject.GetComponent<Animation>().IsPlaying("punch")|| collision.gameObject.GetComponent<Animation>().IsPlaying("kick")))
+        {
+            this.lifePoints--;
+            if (this.lifePoints == 0)
+            {
+                this.animatorMonster.Play("die");
+                this.gameObject.SetActive(false);
+            }
+        }
+       
+    }
+  
+    
+}
---
+++ FinalGame3DINFO551DaniloAndradeMendozaUnityProject/Assets/Scripts/Dragon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Dragon : MonoBehaviour {
+    protected int lifePoints;
+    protected Animator animatorMonster;
+    protected IEnumerator coroutine;
+    protected float waitTime;
+    protected BoxCollider boxColliderDragon;
+    protected float zNewBoxCollider;
+    protected float zStartBoxCollider;
+    public void Start()
+    {
+        lifePoints = 10;
+        animatorMonster = GetComponent<Animator>();
+        waitTime = 15f;
+        boxColliderDragon = GetComponent<BoxCollider>();
+        zNewBoxCollider = 2.5f;
+        zStartBoxCollider = 1f;
+    }
+    public abstract void Update();
+    public abstract IEnumerator WaitAndAttack(float waitTime);
+    public abstract void OnCollisionEnter(Collision collision);
+}
