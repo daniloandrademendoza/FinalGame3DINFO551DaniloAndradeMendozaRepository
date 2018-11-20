4e8beb01aeb40f072924393c2069e86e52a0ed0e
Danilo Andrade Mendoza <daniloandradem10@email.arizona.edu>
2018-11-19 21:47:12 -0700

    Finished the LoadOnClick script and implemented it in the play and exit buttons of the main menu scene, added buttons to the game scene and implemented the LoadOnClick script on them.

---
+++ FinalGame3DINFO551DaniloAndradeMendozaUnityProject/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+    public void LoadScene(int level)
+    {
+        if(level==0)
+        {
+            Application.Quit();
+        }
+        else if(level==1)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        else if (level == 2)
+        {
+            SceneManager.LoadScene("Game");
+        }
+        else if (level == 3)
+        {
+            SceneManager.LoadScene("Controls");
+        }
+    }
+}
