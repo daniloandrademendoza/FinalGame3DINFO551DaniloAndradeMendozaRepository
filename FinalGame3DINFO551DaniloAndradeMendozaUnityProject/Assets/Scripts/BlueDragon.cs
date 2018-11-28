db1d9d40a5901e492e0589119f2b830d9123bfeb
Danilo Andrade Mendoza <daniloandradem10@email.arizona.edu>
2018-11-28 12:25:57 -0700

    Imported the DragonTerrorBringerHP dragons to replace the Rhinos since my game is a dragon hunter game, finished the Blue Dragon script who extends the dragon class and when the player hits the blue dragon he is returned to the main entrance implementing polymorphism.

---
+++ FinalGame3DINFO551DaniloAndradeMendozaUnityProject/Assets/Scripts/BlueDragon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueDragon : Dragon {
+    public override void Update()
+    {
+        StartCoroutine(WaitAndAttack(this.waitTime));
+    }
+    public override IEnumerator WaitAndAttack(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        this.animatorMonster.Play("Basic Attack");
+        this.boxColliderDragon.size = new Vector3(10f,10f,14f);
+    }
+    public void SendToEntrance(Collision collision)
+    {
+        collision.transform.position = new Vector3(35.68f, 0f, 65.1f);
+    }
+    public override void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "MAX" && (collision.gameObject.GetComponent<Animation>().IsPlaying("punch") || collision.gameObject.GetComponent<Animation>().IsPlaying("kick")))
+        {
+            this.lifePoints--;
+            Debug.Log(this.lifePoints);
+            SendToEntrance(collision);
+            if (this.lifePoints == 0)
+            {
+                this.animatorMonster.Play("Die");
+                this.gameObject.SetActive(false);
+            }
+        }
+        else if (collision.gameObject.name == "MAX")
+        {
+            Debug.Log("collision");
+            SendToEntrance(collision);
+        }
+    }
+}
